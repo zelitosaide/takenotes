f55401cebb306640cf859d48cf22b11a3b64b8eb
zelitosaide <zelitosaide83@gmail.com>
2022-10-14 09:57:57 +0200

    playlist items implemented

--- src/routes/playlist.js
+++ src/routes/playlist.js
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
 import { Form, useLoaderData } from "react-router-dom";
 
 import { API } from "../api/client";
+
+const BASE_URL = process.env.REACT_APP_YOUTUBE_BASE_URL;
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export async function loader({ params }) {
   const { playlistId } = params;
@@ -10,6 +14,17 @@ export async function loader({ params }) {
 
 export function PlayList() {
   const { data: playlist } = useLoaderData();
+  const [playlistItems, setPlaylistItems] = useState([]);
+
+  useEffect(function () {
+    fetch(`${BASE_URL}?part=snippet&key=${API_KEY}&playlistId=${playlist.playlistId}&maxResults=50`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function ({ items }) {
+        setPlaylistItems(items);
+      });
+  }, [playlist.playlistId]);
 
   return (
     <div id="contact"> {/* id="playlist" */}
@@ -18,6 +33,20 @@ export function PlayList() {
           <i>{playlist.title}</i>
           <Favorite playlist={playlist} />
         </h1>
+        {playlistItems.length ? (
+          <ul>
+            {playlistItems.map(function (playlistItem) {
+              const { snippet } = playlistItem;
+              return (
+                <li key={playlistItem.id}>{snippet.title}</li>
+              );
+            })}
+          </ul>
+        ) : (
+          <p>
+            <i>No playlist items</i>
+          </p>
+        )}
         <div>
           <Form action="edit">
             <button type="submit">Edit</button>
