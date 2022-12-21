ecedf6f28463a2e3229d112abb4c5a3797c2cbb2
zelitosaide <zelitosaide83@gmail.com>
2022-12-21 20:30:28 +0200

    takenotes client

--- src/api/client.js
+++ src/api/client.js
@@ -1,3 +1,6 @@
 import axios from "axios";
 
-export const API = axios.create({ baseURL: "https://takenotes-api.herokuapp.com" });
+// export const API = axios.create({ baseURL: "https://takenotes-api.herokuapp.com" });
+export const API = axios.create({ baseURL: "http://localhost:5000" });
+
+export const baseUrl = "http://localhost:5000";
